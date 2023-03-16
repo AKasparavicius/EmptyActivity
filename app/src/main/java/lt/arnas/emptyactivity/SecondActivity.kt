6bff4bc45979885020ac5457e8f4a5d1975f076b
AKasparavicius <kasparavicius.ar@gmail.com>
2023-03-16 15:46:11 +0200

    Add SecondActivity.kt

---
+++ app/src/main/java/lt/arnas/emptyactivity/SecondActivity.kt
@@ -0,0 +1,12 @@
+package lt.arnas.emptyactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
